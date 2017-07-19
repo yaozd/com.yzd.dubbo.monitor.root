0a43dae10077d87744842835c78b1f35c8c638a2
yzd <tedijoteamo@163.com>
2017-07-19 17:31:04 +0800

    update

--- com.yzd.dubbo.monitor.root/1.0-com.yzd.dubbo.monitor.web/src/main/resources/static/content/script/application/index.js
+++ com.yzd.dubbo.monitor.root/1.0-com.yzd.dubbo.monitor.web/src/main/resources/static/content/script/application/index.js
@@ -5,10 +5,12 @@ $(function () {
     initData();
     initeCharts();
     applicationClick();
+    inputFunction();
 });
 //region-初始化数据
 function initData(){
     $.get("/application/getAllAPPAndRelation", function(resultVO){
+        allAppResutMap=resultVO.data;
         dataToView(resultVO.data);
     });
 }
@@ -239,16 +241,163 @@ function applicationClick(){
         } else {
             $("#services_section").removeClass("hidden");
             $("#alert_section").html("");
-            //initServiceTable(appName);
+            //创建应用的服务列表
+            initServiceTable(appName);
             //aPPRelationForceChart(appName);
             //服务数据图表出现
             $("#tab_app_data_btn").removeClass("hidden");
             $("#tab_app_ranking_btn").removeClass("hidden");
         }
-        console.log(scroll_offset.top);
         $('body').animate({
             scrollTop: scroll_offset.top-100  //让body的scrollTop等于pos的top，就实现了滚动
         }, 1200);
     });
 }
+//===========================二级方法=================
+// 获得该app的service列表
+function initServiceTable(appName) {
+    var appMap = allAppResutMap.allApp;
+    var appBO = appMap[appName];
+    var serviceMap = appBO.serviceMap;
+
+    var tabs_list = [];
+    var tab_first = true;
+
+    var content_list = [];
+    var tab_content_first = true;
+    var status_name = {'online':'线上','test':'测试','local':'本地','wrong':'错误异常'}
+    $.each(serviceMap,function(status,serviceSet){
+        //标签头
+        var tab_map = {'name':status_name[status],'status':status,'class':''};
+        if(tab_first){
+            tab_map['class'] = 'active';
+            tab_first = false;
+        }
+        tabs_list.push(tab_map);
+        //内容
+        var content_map = {'name':status_name[status],'status':status,'class':''};
+        if(tab_content_first){
+            content_map['class'] = 'active';
+            tab_content_first = false;
+        }
+        if(status == 'wrong'){
+            content_map['wrong'] = 'wrong';
+        }
+
+        //拼接每一行的内容
+        var indexs = 0;
+        var map = {
+            list: serviceSet,
+            indexFunc: function () {
+                return indexs += 1
+            },
+            consumersFunc: function () {
+                var isConsumers = this.isConsumer;
+                if (isConsumers) return '<span class="badge badge-danger"><i class="fa fa-check"></i> </span>'
+            },
+            wrongFunc:function(){
+                var wrongReason = this.wrongReason;
+                if (wrongReason != undefined && wrongReason!= ''){
+                    return wrongReason;
+                }
+            },
+            methodFunc:function(){
+                var html = "";
+                var methodsHost = this.methodsHost;
+                $.each(methodsHost,function(method,hostSet){
+                    html += method +"-----";
+                    $.each(hostSet,function(i,host){
+                        html += host.hostString+" ";
+                    })
+                })
+                return html;
+            },
+            hostFunc:function(){
+                var html = "";
+                var methodsHost = this.methodsHost;
+                $.each(methodsHost,function(method,hostSet){
+                    html += "-----";
+                    $.each(hostSet,function(i,host){
+                        html += host.hostString+" ";
+                    })
+                })
+                return html;
+            }
+
+        };
+        var tbody_html = Mustache.render($('#service_table_tbody_template').html(), map);
+        content_map['tbody_html'] = tbody_html;
+        content_list.push(content_map);
+    });
+
+    var tab_html = Mustache.render($('#service_tab_templates').html(), {'list':tabs_list});
+    var content_html = Mustache.render($('#service_content_templates').html(), {'list':content_list});
+
+    $('#all_service_div').html(tab_html+content_html);
+
+    $("#services_app_span").text(appName);
+
+    $(".service").unbind("click").click(function () {
+        $(this).next("tr").toggleClass("hidden");
+        return false;
+    });
+    $(".service_tab").unbind("click").click(function () {
+        var content_id = $(this).find('a').attr("href");
+
+        $('.service_tab').removeClass("active");
+        $(this).addClass("active");
+        $('.service_content').removeClass("active");
+        $(content_id).addClass("active");
+
+        //重置筛选
+        $("#search_service_value").val('');
+        $(".service").removeClass("hidden");
+        return false;
+    });
+    $('.Tooltip').tooltip()
+}
+// 筛选主表格的app名称
+function filterAppTable() {
+    var key_value = $("#search_app_value").val().trim().toUpperCase();
+    var all_tr = $('#main_application_tbody > tr');
+    if (key_value == '') {
+        $(all_tr).removeClass("hidden");
+    } else {
+        $.each(all_tr, function (i, obj) {
+            var value = $(obj).data("appname").toUpperCase();
+            if (value.indexOf(key_value) == -1) {
+                $(obj).addClass("hidden");
+            } else {
+                $(obj).removeClass("hidden");
+            }
+        });
+    }
+    return false;
+}
 //endregion-点击应用
+
+function inputFunction() {
+
+    $('#search_app_value').keyup(function () {
+        filterAppTable();
+        return false;
+    });
+    $('#search_service_value').keyup(function () {
+        var key_value = $("#search_service_value").val().trim().toUpperCase();
+        var all_tr = $('#all_service_div > div > div.active>div>table>tbody>tr.service');
+        if (key_value == '') {
+            $(all_tr).removeClass("hidden");
+        } else {
+            $.each(all_tr, function (i, obj) {
+                var value = $(obj).data("servicename").toUpperCase();
+                console.log(value);
+                if (value.indexOf(key_value) == -1) {
+                    $(obj).addClass("hidden");
+                } else {
+                    $(obj).removeClass("hidden");
+                }
+            });
+        }
+        return false;
+    });
+};
